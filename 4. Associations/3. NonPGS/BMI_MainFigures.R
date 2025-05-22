ca12d0fd76c8b017f278e4fffe09cec16fcab6ab
walkeralicia <alicia.walker@psych.ox.ac.uk>
2025-05-22 17:30:06 +0100

    exposome

---
+++ 4. Associations/3. NonPGS/BMI_MainFigures.R
@@ -0,0 +1,162 @@
+
+library(ggplot2)
+library(dplyr)
+
+custom_colors <- c("No" = "black",
+                   "Yes" = "#2C7FB8")
+
+#-- Antidepressant Treatment Class Level analysis
+
+#-- BMI unadjusted
+bmi_unadj <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_Results.xlsx", sheet = 8)
+bmi_unadj <- bmi_unadj %>%
+  fill(Outcome, Threshold, ReferenceTerm, .direction = "down") %>% 
+  filter(Term != "AGE" & Term != "SEX") %>%
+  filter(Threshold == "360 days") %>%
+  filter(Outcome == "BMI") %>%
+  filter(ReferenceTerm == "SSRI") %>%
+  select(Outcome, ReferenceTerm, Term, Estimate, `Std..Error`, `t.value`, `Pr...t..`, Sig_FDR, Sig_Bonf) %>%
+  mutate(Adjusted = "No") %>%
+  filter(Term != "SSRI")
+
+#-- BMI adjusted by BMI PGS
+bmi_adj <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_Results.xlsx", sheet = 12)
+bmi_adj <- bmi_adj %>%
+  fill(Outcome, Threshold, ReferenceTerm, .direction = "down") %>% 
+  filter(Term != "sd_pgs" & Term != "(Intercept)") %>%
+  filter(Term != "AGE" & Term != "SEX") %>%
+  filter(Threshold == "360 days") %>%
+  filter(Outcome == "BMI") %>%
+  filter(ReferenceTerm == "SSRI") %>%
+  select(Outcome, ReferenceTerm, Term, Estimate, `Std..Error`, `t.value`, `Pr...t..`, Sig_FDR, Sig_Bonf) %>%
+  mutate(Adjusted = "Yes")
+
+#-- Combine BMI adjusted and unadjusted analysis into one dataframe
+dat <- rbind(bmi_adj, bmi_unadj)
+
+#-- Prepare data for plotting
+dat$Term <- factor(dat$Term, levels = rev(c( "Various","BIP-L" ,"BIP+L", "TeCA", "TCA", "SNRI")))
+dodge_width <- 0.5
+
+#-- Main Plot 
+main_p <- ggplot(dat, aes(x = Term, y = Estimate, group = Adjusted, color = Adjusted)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 3, shape = 18) +  
+  geom_errorbar(aes(ymin = Estimate - (1.96 * `Std..Error`), ymax = Estimate + (1.96 * `Std..Error`)), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 0.5, width = 0) +
+  geom_hline(yintercept = 0, linetype = "longdash", linewidth = 0.5, color = "#333333") +
+  xlab("Antidepressant Class") +
+  ylab(expression(atop("BMI (SSRI as reference)", paste("(", pm, " 95% CI)")))) +
+  labs(color = "Covariate\nBMI PGS") +
+  scale_color_manual(values = custom_colors) +
+  theme_classic() +
+  theme(
+    axis.text.y = element_text(color = "black"),
+    axis.text.x = element_text(color = "black", face = "bold"),
+    legend.position = "right", 
+    legend.title = element_text(face = "bold"),
+    axis.title = element_text(face = "bold")
+  )
+
+main_p
+
+###################################################
+
+
+# Combine the quantitative plots
+
+main_p_expanded <- main_p + 
+  # Increase right margin - adjust the value (currently 3cm) as needed
+  theme(plot.margin = margin(5, 200, 5, 40, "pt"))  # top, right, bottom, left
+
+combined_plot <- plot_grid(
+  p, main_p_expanded,
+  ncol = 1,
+  labels = c("A", "B"),
+  rel_heights = c(2, 1.1)
+)
+
+combined_plot
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\2. PGS\\Results\\AGDS_MF_PGS_plots.png", 
+       plot = combined_plot, device = "png", width = 230, height = 200, units = "mm", dpi=600)
+
+#####################################################
+
+#-- Antidepressant Treatment Level analysis
+
+custom_colors <- c("No" = "black",
+                   "Yes" = "#2C7FB8")
+
+#-- BMI unadjusted
+bmi_unadj <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_Results.xlsx", sheet = 9)
+bmi_unadj <- bmi_unadj %>%
+  fill(Outcome, Threshold, ReferenceTerm, .direction = "down") %>% 
+  filter(Term != "AGE" & Term != "SEX") %>%
+  filter(Outcome == "BMI") %>%
+  filter(ReferenceTerm == "SSRI:Sertraline") %>%
+  select(Threshold, Outcome, ReferenceTerm, Term, Estimate, `Std..Error`, `t.value`, `Pr...t..`, Sig_FDR, Sig_Bonf) %>%
+  mutate(Adjusted = "No") %>%
+  filter(Term != "SSRI:Sertraline")
+
+#-- BMI adjusted by BMI PGS
+bmi_adj <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_Results.xlsx", sheet = 13)
+bmi_adj <- bmi_adj %>%
+  fill(Outcome, Threshold, ReferenceTerm, .direction = "down") %>% 
+  filter(Term != "sd_pgs" & Term != "(Intercept)") %>%
+  filter(Term != "AGE" & Term != "SEX") %>%
+  filter(Outcome == "BMI") %>%
+  filter(ReferenceTerm == "SSRI:Sertraline") %>%
+  select(Threshold, Outcome, ReferenceTerm, Term, Estimate, `Std..Error`, `t.value`, `Pr...t..`, Sig_FDR, Sig_Bonf) %>%
+  mutate(Adjusted = "Yes")
+
+#-- Combine BMI adjusted and unadjusted analysis into one dataframe
+dat <- rbind(bmi_adj, bmi_unadj)
+
+#-- Prepare data for plotting
+dat$Term <- factor(dat$Term, levels = rev(c( "Various", "Combination", "BIP-L", "BIP+L", "TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat$Threshold <- factor(dat$Threshold, levels = c("360 days", "600 days"))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+dodge_width <- 0.5
+
+#-- Main Plot 
+main_p <- ggplot(dat_labeled, aes(x = Term, y = Estimate, group = Adjusted, color = Adjusted)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 3, shape = 18) +  
+  geom_errorbar(aes(ymin = Estimate - (1.96 * `Std..Error`), ymax = Estimate + (1.96 * `Std..Error`)), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 0.5, width = 0) +
+  geom_hline(yintercept = 0, linetype = "longdash", linewidth = 0.5, color = "#333333") +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = (Estimate + (1.96 * `Std..Error`) + 0.01)), 
+            size = 6, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(. ~Threshold, nrow = 2) +
+  xlab("Treatment Group") +
+  ylab(expression(atop("BMI (SSRI:Sertraline as reference)", paste("(", pm, " 95% CI)")))) +
+  labs(color = "Covariate\nBMI PGS") +
+  scale_color_manual(values = custom_colors) +
+  theme_classic() +
+  theme(
+    axis.text.y = element_text(color = "black"),
+    axis.text.x = element_text(color = "black", face = "bold", angle = 60, hjust = 1),
+    legend.position = "right", 
+    legend.title = element_text(face = "bold"),
+    axis.title = element_text(face = "bold")
+  )
+
+main_p
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\2. PGS\\Results\\AGDS_SF_PGS_plots.png", 
+       plot = main_p, device = "png", width = 150, height = 200, units = "mm", dpi=600)
+
---
+++ 4. Associations/3. NonPGS/SupplementaryFigures_nonPGS.R
@@ -0,0 +1,375 @@
+
+#-- Load R libraries
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+custom_colors <- c("360 days" = "#4169E1",
+                   "600 days" = "#004400")
+
+#====================== Pharmaceutical Plot =========================================
+
+#-- Read in results in which the dependent variable is quantitative
+dat <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_results.xlsx", sheet = 9) %>%
+  fill(Threshold, ReferenceTerm, Outcome, Total_N, .direction = "down")
+
+dat <- dat %>% filter(Term != "AGE" & Term != "SEX" & ReferenceTerm == "SSRI:Sertraline" & Term != "SSRI:Sertraline",
+                        Outcome %in% c("Average Prescription Episode Length", "Number of Prescription Episodes",
+                                       "Total Prescription Dispense", "Number of Unique Antidepressants", "Number of Unique AD Class"))
+
+#-- Renaming treatment and PGS labels
+dat$Term <- factor(dat$Term, levels = rev(c("TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat$Threshold <- factor(dat$Threshold, levels = c("360 days", "600 days"))
+
+variable_order <- c("Total Prescription Dispense",
+                    "Number of Prescription Episodes",
+                    "Average Prescription Episode Length",
+                    "Number of Unique Antidepressants",
+                    "Number of Unique AD Class")
+dat_factored <- dat %>%
+  mutate(Outcome = factor(Outcome, levels = variable_order))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat_factored %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+
+# Plotting code
+dodge_width <- 0.6
+p <- ggplot(dat_labeled, aes(x = Term, y = Estimate, group = Threshold, colour = Threshold)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 5) +  
+  geom_errorbar(aes(ymin = Estimate - (1.96 *  `Std..Error`), ymax = Estimate + (1.96 * `Std..Error`)), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 1.5, width = 0) +
+  geom_hline(yintercept = 0) +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = (Estimate + (1.96 * `Std..Error`) + 0.01)), 
+            size = 10, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(~Outcome, ncol = 3, scales = "free_y") +
+  xlab("Treatment group") +
+  ylab("Estimate difference from Sertraline") +
+  theme_bw(base_size = 27) +
+  scale_color_manual(values = custom_colors) +
+  theme(
+    strip.text = element_text(color = "black"), 
+    strip.background = element_rect(fill = "white"),
+    axis.text.x = element_text(color = "black", angle = 90, vjust = 0.7),
+    axis.text.y = element_text(color = "black"),
+    legend.position = "top"
+  )
+
+p
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\3. NonPGS\\Results\\Figures\\PharmaceuticalMetrics_QuantitativeAssociations.png", 
+       plot = p, device = "png", width = 500, height = 400, units = "mm")
+
+
+
+#======================== Quantitative survey phenotypes =================================
+
+#-- Read in results in which the dependent variable is quantitative
+dat <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_results.xlsx", sheet = "Table11") %>%
+  rename(
+    Threshold = threshold,
+    ReferenceTerm = reference_term, 
+    Outcome = outcome, 
+    Term = term,
+    Estimate = estimate, 
+    `Std..Error` = std.error,
+    Sig_Bonf = sig_bonf, 
+    Sig_FDR = sig_fdr,
+    Total_N = total_n
+  ) %>%
+  fill(Threshold, ReferenceTerm, Outcome, Total_N, .direction = "down")
+dat <- dat %>% filter(Term != "AGE" & Term != "SEX" & ReferenceTerm == "SSRI:Sertraline" & Term != "SSRI:Sertraline",
+                      Outcome %in% c("Age", "Age of MDD Onset", "Times 2-Weeks of MDD", "BMI", "Education Level",
+                                     "Physical health", "Drinks over 3 Months"))
+
+#-- Renaming treatment and PGS labels
+dat$Term <- factor(dat$Term, levels = rev(c( "Various", "Combination", "BIP-L", "BIP+L", "TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat$Threshold <- factor(dat$Threshold, levels = c("360 days", "600 days"))
+
+variable_order <- c("Age",
+                    "Age of MDD Onset",
+                    "Times 2-Weeks of MDD",
+                    "BMI",
+                    "Education Level",
+                    "Physical health",
+                    "Drinks over 3 Months")
+dat_factored <- dat %>%
+  mutate(Outcome = factor(Outcome, levels = variable_order))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat_factored %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+
+# Plotting code
+dodge_width <- 0.5
+p <- ggplot(dat_labeled, aes(x = Term, y = Estimate, group = Threshold, colour = Threshold)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 4) +  
+  geom_errorbar(aes(ymin = Estimate - (1.96 *  `Std..Error`), ymax = Estimate + (1.96 * `Std..Error`)), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 1, width = 0) +
+  geom_hline(yintercept = 0) +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = (Estimate + (1.96 * `Std..Error`) + 0.01)), 
+            size = 10, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(~Outcome, nrow = 2, scales = "free_y") +
+  xlab("Treatment group") +
+  ylab("Estimate difference from Sertraline") +
+  theme_bw(base_size = 27) +
+  scale_color_manual(values = custom_colors) +
+  theme(
+    strip.text = element_text(color = "black"), 
+    strip.background = element_rect(fill = "white"),
+    axis.text.x = element_text(color = "black", angle = 90, vjust = 0.7),
+    axis.text.y = element_text(color = "black"),
+    legend.position = "top",
+    legend.title = element_blank()
+  )
+
+p
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\3. NonPGS\\Results\\Figures\\ClinicalFeatures_QuantitativeAssociations.png", 
+       plot = p, device = "png", width = 500, height = 400, units = "mm")
+
+
+#=================== Binary Worst MDD Episode Symptoms ===============================
+
+#-- Read in results in which the dependent variable is quantitative
+dat <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_results.xlsx", sheet = 7) %>%
+  fill(Threshold, Reference, Dependent, Total_N, .direction = "down")
+
+variable_order <- c("Low Interest 2-Weeks",
+                    "Depressed 2-Weeks",
+                    "Fatigued",
+                    "Guilty Feelings",
+                    "No Focus",
+                    "Death Thoughts",
+                    "Appetite/Weight Change",
+                    "Sleep Disturbances",
+                    "Movement Changes")
+
+dat <- dat %>% filter(Term != "AGE" & Term != "SEX1" & Reference == "SSRI:Sertraline" & Term != "SSRI:Sertraline",
+                      Dependent %in% variable_order)
+
+#-- Renaming treatment and PGS labels
+dat$Term <- factor(dat$Term, levels = rev(c( "Various", "Combination", "BIP-L", "BIP+L", "TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat <- dat %>%
+  mutate(Threshold = ifelse(Threshold == 360, "360 days", "600 days")) %>%
+  mutate(Threshold = factor(Threshold, levels = c("360 days", "600 days")))
+
+
+dat_factored <- dat %>%
+  mutate(Dependent = factor(Dependent, levels = variable_order))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat_factored %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+
+# Plotting code
+dodge_width <- 0.7
+p <- ggplot(dat_labeled, aes(x = Term, y = OR, group = Threshold, colour = Threshold)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 1.5) +  
+  geom_errorbar(aes(ymin = LCI, ymax = UCI), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 0.3, width = 0) +
+  geom_hline(yintercept = 1) +
+  theme_bw() +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = UCI + 0.01), 
+            size = 7, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(~Dependent, nrow = 3, scales = "free_y") +
+  xlab("Treatment group") +
+  ylab("Odds Ratio (95% CI) with SSRI:Sertraline as Reference") +
+  scale_color_manual(values = custom_colors) +
+  theme(
+    strip.text = element_text(color = "black"), 
+    strip.background = element_rect(fill = "white"),
+    axis.text.x = element_text(color = "black", angle = 90, vjust = 0.7),
+    axis.text.y = element_text(color = "black"),
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    plot.title = element_text(color = "black", face = "bold")
+  )
+
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\3. NonPGS\\Results\\Figures\\MDDSymptoms_BinaryAssociations.png", 
+       plot = p, device = "png", width = 220, height = 250, units = "mm", dpi = 600)
+
+
+############################### Clinical Binary Survey Phenotypes ############################################
+
+#-- Read in results in which the dependent variable is quantitative
+dat <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_results.xlsx", sheet = 7) %>%
+  fill(Threshold, Reference, Dependent, Total_N, .direction = "down")
+
+
+variable_order <- c("Sex",
+                    "Self-report Responders",
+                    "Self-report Discontinuation", 
+                    "Suicidal Ideation",
+                    "Regular Smoker")
+
+dat <- dat %>% filter(Term != "AGE" & Term != "SEX1" & Reference == "SSRI:Sertraline" & Term != "SSRI:Sertraline",
+                      Dependent %in% variable_order)
+
+#-- Renaming treatment and PGS labels
+dat$Term <- factor(dat$Term, levels = rev(c( "Various", "Combination", "BIP-L", "BIP+L", "TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat <- dat %>%
+  mutate(Threshold = ifelse(Threshold == 360, "360 days", "600 days")) %>%
+  mutate(Threshold = factor(Threshold, levels = c("360 days", "600 days")))
+
+
+dat_factored <- dat %>%
+  mutate(Dependent = factor(Dependent, levels = variable_order))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat_factored %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+
+# Plotting code
+dodge_width <- 0.7
+p <- ggplot(dat_labeled, aes(x = Term, y = OR, group = Threshold, colour = Threshold)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 4) +  
+  geom_errorbar(aes(ymin = LCI, ymax = UCI), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 1, width = 0) +
+  geom_hline(yintercept = 1) +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = UCI + 0.01), 
+            size = 10, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(~Dependent, nrow = 2, scales = "free_y") +
+  xlab("Treatment group") +
+  ylab("Odds Ratio (95% CI) with Sertraline as Reference") +
+  theme_bw(base_size = 27) +
+  scale_color_manual(values = custom_colors) +
+  theme(
+    strip.text = element_text(color = "black"), 
+    strip.background = element_rect(fill = "white"),
+    axis.text.x = element_text(color = "black", angle = 90, vjust = 0.7),
+    axis.text.y = element_text(color = "black"),
+    legend.position = "top",
+    legend.title = element_blank()
+  )
+
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\3. NonPGS\\Results\\Figures\\ClinicalFeatures_BinaryAssociations.png", 
+       plot = p, device = "png", width = 500, height = 400, units = "mm")
+
+#================= Binary Comorbidities ================================================
+
+#-- Read in results in which the dependent variable is quantitative
+dat <- read_excel("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\All_results.xlsx", sheet = 7) %>%
+  fill(Threshold, Reference, Dependent, Total_N, .direction = "down")
+
+
+variable_order <- c("Type 2 Diabetes", 
+                    "Stomach Ulcers",
+                    "PDMD",
+                    "Anxiety Disorder",
+                    "Personality Disorder",
+                    "Substance Use Disorder",
+                    "ADHD",
+                    "Obsessive-compulsive Disorder",
+                    "Seasonal Affective Disorder",
+                    "Premenstrual Dysphoric Disorder",
+                    "Back pain",
+                    "Chronic Fatigue Syndrome",
+                    "Epilepsy",
+                    "Chronic pain",
+                    "Migraines or Headaches",
+                    "Endometriosis",
+                    "PCOS",
+                    "Fibroids (uterus)")
+
+dat <- dat %>% filter(Term != "AGE" & Term != "SEX1" & Reference == "SSRI:Sertraline" & Term != "SSRI:Sertraline",
+                      Dependent %in% variable_order)
+
+#-- Renaming treatment and PGS labels
+dat$Term <- factor(dat$Term, levels = rev(c( "Various", "Combination", "BIP-L", "BIP+L", "TeCA:Mirtazapine", "TCA:Amitriptyline", "SNRI:Duloxetine", "SNRI:Desvenlafaxine", "SNRI:Venlafaxine", "SSRI:Paroxetine", "SSRI:Fluoxetine", "SSRI:Escitalopram", "SSRI:Citalopram")))
+dat <- dat %>%
+  mutate(Threshold = ifelse(Threshold == 360, "360 days", "600 days")) %>%
+  mutate(Threshold = factor(Threshold, levels = c("360 days", "600 days")))
+
+
+dat_factored <- dat %>%
+  mutate(Dependent = factor(Dependent, levels = variable_order))
+
+#-- Label for FDR or Bonferroni significance
+dat_labeled <- dat_factored %>%
+  mutate(
+    Sig_label = case_when(
+      Sig_Bonf == "*" ~ "**",
+      Sig_FDR == "*" & is.na(Sig_Bonf) ~ "*",
+      TRUE ~ NA
+    )
+  )
+
+
+# Plotting code
+dodge_width <- 0.7
+p <- ggplot(dat_labeled, aes(x = Term, y = OR, group = Threshold, colour = Threshold)) +
+  geom_point(position = position_dodge(width = dodge_width), size = 4) +  
+  geom_errorbar(aes(ymin = LCI, ymax = UCI), 
+                position = position_dodge(width = dodge_width), 
+                linewidth = 1, width = 0) +
+  geom_hline(yintercept = 1) +
+  geom_text(aes(label = Sig_label, 
+                x = Term, 
+                y = UCI + 0.01), 
+            size = 10, color = "black", 
+            position = position_dodge(width = dodge_width)) +  
+  facet_wrap(~Dependent, nrow = 6, scales = "free_y") +
+  xlab("Treatment group") +
+  ylab("Odds Ratio (95% CI) with Sertraline as Reference") +
+  theme_bw(base_size = 27) +
+  scale_color_manual(values = custom_colors) +
+  theme(
+    strip.text = element_text(color = "black"), 
+    strip.background = element_rect(fill = "white"),
+    axis.text.x = element_text(color = "black", angle = 90, vjust = 0.7),
+    axis.text.y = element_text(color = "black"),
+    legend.position = "top",
+    legend.title = element_blank()
+  )
+
+
+ggsave("C:\\Users\\walkera\\OneDrive - Nexus365\\Documents\\PhD\\AGDS\\Pharmacogenomics\\4. Associations\\3. NonPGS\\Results\\Figures\\Comorbidities_BinaryAssociations.png", 
+       plot = p, device = "png", width = 500, height = 650, units = "mm")
+
+
